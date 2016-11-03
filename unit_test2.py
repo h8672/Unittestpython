923c87952534d2b2f906bddc6c162d1cea246efc
Juha-Matti Kokkonen <juhamk91@gmail.com>
2016-11-03 13:49:12 +0200

    Update unit_test2.py

--- unit_test2.py
+++ unit_test2.py
@@ -1,1 +1,10 @@
 import unittest
+from 2dgame import 2Dgame
+
+class GameTests(unittest.TestCase):
+  def change_title(self):
+    self.assertTrue(isinstance(newgame.get_title(), str);
+  
+  
+if __name__ == '__main__':
+  unittest.main()
