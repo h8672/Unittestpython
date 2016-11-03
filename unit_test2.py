7cacff8c8e508c033180719e816cca640f56c129
Juha-Matti Kokkonen <juhamk91@gmail.com>
2016-11-03 13:57:14 +0200

    Update unit_test2.py

--- unit_test2.py
+++ unit_test2.py
@@ -1,10 +1,22 @@
 import unittest
+
 from dgame import Dgame
 
 class GameTests(unittest.TestCase):
-  def change_title(self):
-    self.assertTrue(isinstance(newgame.get_title(), str);
+    
+    #Setup method to create test object
+    def setUp(self):
+        print "Setting up!"
+        self.newgame = Dgame("titteli")
+
+    #Teardown method to delete the test object
+    def tearDown(self):
+        print "Tearing down!"
+        del self.newgame
+        
+    def test_title_is_string(self):
+        self.assertTrue(isinstance(self.newgame.get_title(), str), "test failed! title not string!")
   
   
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
